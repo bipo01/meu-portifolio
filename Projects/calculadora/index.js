5656ab74e28b1f62b6fc725c02f3627aadf2a9c6
joao bispo <joao.s.bispo16@gmail.com>
2025-02-04 17:23:01 -0400

    calculadora ajustada

--- Projects/calculadora/index.js
+++ Projects/calculadora/index.js
@@ -1,17 +1,48 @@
 const display = document.getElementById("display");
+const caracteres = ["+", "-", "*", "/"];
 
 function appendToDisplay(input) {
+  if (display.value == "Erro") {
+    display.value = "";
+  }
+
+  if (caracteres.includes(input)) {
+    if (isFinite(display.value.split("").at(-1))) {
+      display.value += input;
+    }
+  } else if (input != ".") {
     display.value += input;
+  }
+
+  if (
+    display.value
+      .split(/[\/\+\-\*]/)
+      .at(-1)
+      .includes(".")
+  ) {
+    console.log("Existe");
+  } else {
+    if (input == ".") {
+      if (isFinite(display.value.split("").at(-1))) {
+        display.value += input;
+      }
+    }
+  }
 }
 
 function clearDisplay() {
-    display.value = "";
+  display.value = "";
 }
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Erro";
+    setTimeout(() => {
+      if (display.value == "Erro") {
+        display.value = "";
+      }
+    }, 1500);
+  }
 }
